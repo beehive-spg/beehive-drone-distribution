a9a4797889e2c77aff5187aa87d19537ab5247cc
Dschonas <jonasneumair@gmail.com>
2017-12-16 15:51:03 +0100

    Delete and replace files during restructuring

--- src/tests/test_log_receiver.py
+++
@@ -1,5 +0,0 @@
-from log_receiver import TLR
-
-test_log_receiver = TLR()
-test_log_receiver.start_queue()
-test_log_receiver.start_channel()
--- src/tests/test_worker.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '../rabbitmq/consumer/')
-from worker import Worker
-import threading
-
-worker = Worker('workload_prediction_queue')
-worker.start()

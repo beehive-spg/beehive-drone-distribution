62ac75c5843c0bce377be03ef248421ff39ce032
Dschonas <jonasneumair@gmail.com>
2018-01-21 15:24:40 +0100

    Refine training process

--- drone_distribution/training.py
+++ drone_distribution/training.py
@@ -24,40 +24,53 @@ def main(accurracy):
 	train(accurracy)
 
 def setup_training():
-	global moved_drones_before, weighted_hives, changed_hive
+	global moved_drones_before, weighted_hives, changed_hive, flagged_hives
 	changed_hive = None
-	moved_drones_before = tr.request_number_of_moved_drones()
-	logging.info("number of moved drones before: " + str(moved_drones_before))
 	# retrieve the list of hives with weights
 	weighted_hives = { 11:1.1, 22:1.0}
 	logging.info("list of hives: " + str(weighted_hives))
+	# run system
+	moved_drones_before = tr.request_number_of_moved_drones()
+	logging.info("number of moved drones before: " + str(moved_drones_before))
 
 def train(accurracy):
-	# run the whole system with the test data
-	logging.info("training completed")
-	moved_drones_after = tr.request_number_of_moved_drones()
-	movde_drones = get_moved_drones(moved_drones_after)
-	logging.info("difference: " + str(movde_drones))
-	if (movde_drones > 0):
-		number_of_moved_drones = get_moved_drones(moved_drones_after)
-		logging.info("training improved by: " + str(number_of_moved_drones))
-	elif (changed_hive != None):
-		weighted_hives[changed_hive] -= accurracy
-		flagged_hives.append(changed_hive)
-		logging.info("rollback weight change")
-	adjust_weights(accurracy)
+	while (len(flagged_hives) != len(weighted_hives)):
+		adjust_weights(accurracy)
+		# run the whole system with the test data
+		logging.info("training completed")
+		moved_drones_after = tr.request_number_of_moved_drones()
+		movde_drones = get_moved_drones(moved_drones_after)
+		logging.info("difference: " + str(movde_drones))
+		if (movde_drones > 0):
+			number_of_moved_drones = get_moved_drones(moved_drones_after)
+			logging.info("training improved by: " + str(number_of_moved_drones))
+			reset_flagged_hives()
+		elif (changed_hive != None):
+			weighted_hives[changed_hive] -= accurracy
+			flagged_hives.append(changed_hive)
+			logging.info("rollback weight change")
+			if (len(flagged_hives) == len(weighted_hives)):
+			logging.info("no more training necessary")
+			break;
 
 def adjust_weights(accurracy):
 	# list with hives which weight changes didn't improve the system
 	flagged_hives = {22}
-	for hive in weighted_hives:
-		if (hive not in flagged_hives and random.randrange(0,2) == 1):
-			weighted_hives[hive] += accurracy
-			changed_hive = hive
-			break;
-	if (len(flagged_hives) == len(weighted_hives)):
-		logging.info("no more training necessary")
-		return
+	range_of_hives = len(weighted_hives)
+	possible_hives = get_possible_hives()
+	random_hive = random.randrange(len(possible_hives))
+	weighted_hives[random_hive] += accurracy
+	changed_hive = random_hive
 
 def get_moved_drones(after):
 	return after - moved_drones_before
+
+def get_possible_hives():
+	possible_hives = []
+	for hive in weighted_hives:
+		if (hive not in flagged_hives):
+			possible_hives.append(hive)
+	return possible_hives
+
+def reset_flagged_hives():
+	flagged_hives = []

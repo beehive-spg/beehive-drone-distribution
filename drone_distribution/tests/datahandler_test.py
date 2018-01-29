ab53e9d95f902dc3e50aebe05688a7dde13212a4
Dschonas <jonasneumair@gmail.com>
2018-01-29 13:00:23 +0100

    Add datahandler tests

--- drone_distribution/tests/datahandler_test.py
+++ drone_distribution/tests/datahandler_test.py
@@ -10,6 +10,32 @@ def test_get_workload_in(mock_orders, mock_drones):
 	workload = datahandler.get_workload_in(1,1)
 	assert workload == 5
 
+@patch('drone_distribution.datahandler.get_time_of_impact')
+@patch('drone_distribution.datahandler.get_drones_in')
+@patch('drone_distribution.datahandler.get_orders_in')
+@patch('drone_distribution.datahandler.get_workload_in')
+def test_amount_of_drones_for(mock_workload, mock_orders,
+								mock_drones, mock_impact_time):
+	workload = 0.5
+	orders = 50
+	drones = 100
+	mock_workload.return_value = workload
+	mock_orders.return_value = orders
+	mock_drones.return_value = drones
+	mock_impact_time.side_effect = [ 100, 100 ]
+	expected_amount = drones - (orders / workload)
+	amount_of_drones = datahandler.get_amount_of_drones_for(0)
+	assert amount_of_drones == expected_amount
+
+@patch('drone_distribution.test_requests.request_charging_time')
+@patch('drone_distribution.test_requests.request_flying_time')
+def test_get_time_of_impact(mock_flying_time, mock_charging_time):
+	mock_flying_time.return_value = 3
+	mock_charging_time.return_value = 30
+	time_of_impact = datahandler.get_time_of_impact()
+	expected_time = 33
+	assert time_of_impact == expected_time
+
 @patch('drone_distribution.test_requests.request_drones_in')
 def test_get_drones_in(mock_drones):
 	mock_drones.return_value = 10
@@ -38,6 +64,16 @@ def test_get_prediction_status_decreasing(mock_workload, mock_time_of_impact):
 	workload = datahandler.get_prediction_status(1)
 	assert round(workload, 1) == -0.3
 
+@patch('drone_distribution.datahandler.get_time_of_impact')
+@patch('drone_distribution.datahandler.get_workload_in')
+def test_get_sum_of_workload(mock_workload, mock_impact_time):
+	workloads = [ 0.5, 0.6, 0.7 ]
+	mock_workload.side_effect = workloads
+	mock_impact_time.side_effect = [ 10, 10 ]
+	_sum = datahandler.get_sum_of_workload_of(0)
+	expected_sum = workloads[0] + workloads[1] + workloads[2]
+	assert _sum == expected_sum
+
 @patch('drone_distribution.datahandler.get_needed_drones')
 def test_get_drones_to_send_eotd_true(mock_needed_drones):
 	mock_needed_drones.return_value = 10
@@ -49,3 +85,4 @@ def test_get_drones_to_send_eotd_false(mock_drones_without_impact):
 	mock_drones_without_impact.return_value = 10
 	drones_without_impact = datahandler.get_drones_to_send(1, False)
 	assert drones_without_impact == 10
+

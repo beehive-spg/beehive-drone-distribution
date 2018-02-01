c1ca20f85c25fb8c7af513aaea2520ced91bc52f
Dschonas <jonasneumair@gmail.com>
2018-02-01 17:36:56 +0100

    DLN-249 #time 30min Add new queries

--- drone_distribution/rest.py
+++ drone_distribution/rest.py
@@ -2,48 +2,26 @@
 import os
 import requests as r
 
-def get_orders_in(time, _id):
-	orders = r.get(url("/hives/workload/:"+time+"/:"+_id))
-	return orders.json()
+def get_reachable_hives():
+	hives = r.get(url("/reachable"))
+	return hives.json()
 
-def get_drones_in(time, _id):
-	drones = r.get(url("/"))
-	return drones.json()
+def get_types():
+	types = r.get(url("/types"))
+	return types.json()
 
-def get_neighborhood_hives(_id):
-	neighbors = r.get(url("/"))
-	return [11,22,33,44]
-
-def get_flying_time():
-	flying_time = r.get(url("/"))
-	return flying_time.json()
-
-def get_charging_time():
-	charging_time = r.get(url("/"))
-	return charging_time.json()
-
-def get_available_drones(_id):
-	drones = r.get(url("/"))
-	return drones.json()
-
-def get_average_workload():
-	workload = r.get(url("/"))
-	return workload.json()
-
-def get_hive_weight_evaluation():
-	evaluation = r.get(url("/"))
-	return evaluation.json()
-
+# will be implemented
 def get_number_of_moved_drones():
-	drones = r.get(url("/"))
+	drones = r.get(url("/distributions"))
 	return drones.json()
 
 def get_all_hives():
 	hives = r.get(url("/hives"))
 	return hives.json()
 
-def post_hive_weight(weights):
-	r.post(url("/"), str(weights))
+def get_all_hives_with_workload():
+	hives = r.get(url("/hives/workload"))
+	return hives.json()
 
 def post_hive_drone(drone):
 	r.post(url("/drones"), str(drone))
@@ -51,3 +29,20 @@ def post_hive_drone(drone):
 def url(route):
 	host = os.environ.get('DB_URL', os.environ['DBURL'])
 	return host + route
+
+'''
+# TODO: adapt to database
+def get_orders_in(time, _id):
+	orders = r.get(url("/hives/workload/:"+time+"/:"+_id))
+	return orders.json()
+def get_drones_in(time, _id):
+	drones = r.get(url("/"))
+	return drones.json()
+
+# TODO: adapt to new evaluation model
+def get_hive_weight_evaluation():
+	evaluation = r.get(url("/"))
+	return evaluation.json()
+def post_hive_weight(weights):
+	r.post(url("/"), str(weights))
+'''

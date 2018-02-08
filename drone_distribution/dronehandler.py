5d12cc2241175ea31e56da63b69b174d14dbed1b
Dschonas <jonasneumair@gmail.com>
2018-02-08 15:33:04 +0100

    Reformat dronehandler

--- drone_distribution/dronehandler.py
+++ drone_distribution/dronehandler.py
@@ -14,24 +14,24 @@ def get_average_time_of_impact(_id):
 	chargetime = get_chargetime_of_drone(types)
 	return flying_time + chargetime
 
+def get_flying_time(_from, to, types):
+	distance = locationhandler.get_distance_between(_from, to)
+	speed = get_speed_of_drone(types)
+	return distance / speed
+
 def get_average_flying_time(_id, types):
 	distance = locationhandler.get_average_distance_to(_id)
 	speed = get_speed_of_drone(types)
 	return distance / speed
 
-def get_flying_time(_from, to, types):
-	distance = locationhandler.get_distance_between(_from, to)
-	speed = get_speed_of_drone(types)
-	return distance / speed
+def get_chargetime_of_drone(types):
+	return types['chargetime']
 
 def get_speed_of_drone(types):
 	return types['speed']
 
 def get_range_of_drone(types):
 	return types['range']
-
-def get_chargetime_of_drone(types):
-	return types['chargetime']
 
 def get_total_number_of_drones():
 	all_drones = rest.get_all_drones()

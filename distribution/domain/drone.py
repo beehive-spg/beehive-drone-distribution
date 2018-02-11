e3b788ee5fffb30d6cacedd1b6377db60834c3a2
Dschonas <jonasneumair@gmail.com>
2018-02-11 20:58:22 +0100

    DLN-284 #time 20m Changes in domain     - Change status enum to stati     - Add status domain class     - Adapt drone domain to changes

--- distribution/domain/drone.py
+++ distribution/domain/drone.py
@@ -1,14 +1,13 @@
-from distribution.domain import Hive, Status
+from distribution.domain.hive import Hive
+from distribution.domain.type import Type
+from distribution.domain.status import Status
 from schematics.models import Model
 from schematics.types import IntType, StringType
 from schematics.types.compound import ModelType
 
 class Drone(Model):
-	id = IntType(required=True)
+	id = IntType()
 	hive = ModelType(Hive, serialize_when_none=False)
 	name = StringType(serialize_when_none=False)
 	type = ModelType(Type, serialize_when_none=False)
-	status = StringType(choices = [
-							Status.idle.value,
-							Status.flying.value,
-							Status.charging.value ], serialize_when_none=False)
+	status = ModelType(Status, serialize_when_none=False)
--- distribution/domain/status.py
+++ distribution/domain/status.py
@@ -1,10 +1,10 @@
-from enum import Enum
+from distribution.domain.stati import Stati
+from schematics.models import Model
+from schematics.types import IntType, StringType
 
-class Status(Enum):
-	"""
-	GET methods use Status.status.name
-	POST methods use Status.status.value
-	"""
-	idle = "drone.status/idle"
-	flying = "drone.status/flying"
-	charging = "drone.status/charging"
+class Status(Model):
+	id = IntType()
+	ident = StringType(choices = [
+							Stati.idle.value,
+							Stati.flying.value,
+							Stati.charging.value ], serialize_when_none=False)

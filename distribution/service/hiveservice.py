25bf97b273897e3c242bfb72f8cd4c3a0b45198f
Dschonas <jonasneumair@gmail.com>
2018-02-11 21:09:04 +0100

    DLN-284 #time 60m Refactor hiveservice including tests

--- distribution/service/hiveservice.py
+++ distribution/service/hiveservice.py
@@ -1,9 +1,132 @@
 #!/usr/bin/env python3
 import json
-from distribution.domain.point import Point
+from distribution.domain.hive import Hive
+from distribution.domain.drone import Drone
+from distribution.domain.building import Building
 from distribution.rest import rest
-from distribution.service import droneservice, helper
+from distribution.service import buildingservice, droneservice, helper
 
+# eotd true, returns drones needed for the next day
+# eotd false, returns drones needed to harm the network as little as possible
+def get_drones_to_send(hiveid, eotd):
+	if (eotd):
+		return get_needed_drones(hiveid, helper.get_timestamp_for_the_next_day())
+	hives = rest.get_all_buildings()
+	return get_free_drones(hiveid, hives)
+
+# returns number of drones needed(+) or missing(-)
+def get_needed_drones(_id, time):
+	demand = get_drone_demand(time, _id)
+	supply = get_free_drones(time, _id)
+	return demand - supply
+
+def get_drone_demand(hiveid, hives):
+	for hive in hives:
+		hive = Hive(hive)
+		if (hive.id == hiveid):
+			return hive.demand
+	return 99999
+
+# TODO: adapt to database
+# TODO: non existing id? possible?
+def get_free_drones(hiveid, hives):
+	for hive in hives:
+		hive = Hive(hive)
+		if (hive.id == hiveid):
+			return hive.free
+	return -1
+
+def set_d_of_hive(hiveid, hives):
+	incoming = get_incoming_drones(hiveid, hives)
+	outgoing = get_outgoing_drones(hiveid, hives)
+	io_ratio = incoming / outgoing
+	drone_demand = get_drone_demand(hiveid, hives)
+	get_hivedomain(hives)
+	hive = Hive()
+	hive.id = hiveid
+	hive.demand = drone_demand
+	rest.post_desired_drones_of_hive(hive)
+
+# TODO: error code handling, parameter is list of hives
+def get_incoming_drones(hiveid, hives):
+	for hive in hives:
+		if (hive.id == hiveid):
+			return hive.i
+	return 99999
+
+def get_outgoing_drones(hiveid, hives):
+	for hive in hives:
+		if (hive.id == hiveid):
+			return hive.o
+	return 99999
+
+def get_building_of_hive(hiveid):
+	buildings = buildingservice.get_all_buildings()
+	for building in buildings:
+		if (building.hive.id == hiveid):
+			return building.id
+	return -1
+
+def is_giving_drones_now(_id):
+	return is_giving_drones(_id, helper.now())
+
+def is_giving_drones(hiveid, time):
+	number_of_drones = get_needed_drones(hiveid, time)
+	if (number_of_drones > 0):
+		return True
+	return False
+
+def get_all_hive_ids():
+	all_hives = get_all_hives()
+	hives = []
+	for h in all_hives:
+		hive = Hive(h)
+		hives.append(hive.id)
+	return hives
+
+def get_drones_of_hive(hiveid):
+	all_drones = droneservice.get_all_drones()
+	drones = []
+	for drone in all_drones:
+		if (drone.hive.id == hiveid):
+			drones.append(drone)
+	return drones
+
+def get_hives_with_dronecount():
+	all_drones = droneservice.get_all_drones()
+	all_hives = get_all_hives()
+	hives = dict()
+	for hive in all_hives:
+		number_of_drones = 0
+		for drone in all_drones:
+			if (hive.id == drone.hive.id):
+				number_of_drones += 1
+		hives[hive.id] = number_of_drones
+	return hives
+
+#for testing purposes
+def set_drones_for_hive(hiveid, amount):
+	for nr in range(amount):
+		drone = Drone()
+		drone.hive.id = hiveid
+		drone.name = "drone-"+str(nr)
+		drone.status.ident = "drone.status/idle"
+		rest.post_hive_drone(drone)
+
+def get_all_hives():
+	all_buildings = buildingservice.get_all_buildings()
+	hives = []
+	for building in all_buildings:
+		hives.append(building.hive)
+	return hives
+
+def get_hivedomain(json):
+	hive = Hive(json)
+	hive.validate()
+	return hive
+
+
+### --------- Adapt to database
 def get_workload_in(time, _id):
 	orders = get_orders_in(time, _id)
 	drones = get_drones_in(time, _id)
@@ -39,33 +162,9 @@ def get_drones_in(time, _id):
 def get_orders_in(time, _id):
 	return rest.get_orders_in(time, _id)
 
-# eotd true, returns drones needed for the next day
-# eotd false, returns drones needed to harm the network as little as possible
-def get_drones_to_send(_id, eotd):
-	if (eotd):
-		return get_needed_drones(_id, helper.get_timestamp_for_the_next_day())
-	hives = rest.get_all_hives()
-	return get_free_drones(_id, hives)
 
-# TODO: adapt to database
-# TODO: non existing id? possible?
-def get_free_drones(_id, hives):
-	for hive in hives:
-		if (hive['id'] == _id):
-			return hive['hive']['free']
-	return -1
-
-# TODO: refactor
-def get_reachable_buildings(_id):
-	reachable_buildings = rest.get_reachable_buildings()
-	reachable = []
-	for hive in reachable_buildings:
-		if (hive['start']['id'] == _id):
-			reachable.append(hive['end']['id'])
-		elif (hive['end']['id'] == _id):
-			reachable.append(hive['start']['id'])
-	return reachable
-
+# TODO: replace by get reachable building if not needed
+# TODO: if needed, get_building_of_hive will return buildingdomain
 def get_reachable_hives(_id):
 	_id = get_building_of_hive(_id)
 	buildings = get_reachable_buildings(_id)
@@ -77,121 +176,7 @@ def get_reachable_hives(_id):
 				reachable.append(hive['hive']['id'])
 	return reachable
 
-def get_building_of_hive(_id):
-	hives = rest.get_all_hives()
-	for hive in hives:
-		if (hive['hive']['id'] == _id):
-			return hive['id']
-	return -1
-
-def is_giving_drones_now(_id):
-	return is_giving_drones(_id, datetime.datetime.now())
-
-def is_giving_drones(_id, time):
-	number_of_drones = get_needed_drones(_id, time)
-	if (number_of_drones > 0):
-		return True
-	return False
-
-# returns number of drones needed(+) or missing(-)
-def get_needed_drones(_id, time):
-	demand = get_drone_demand(time, _id)
-	supply = get_free_drones(time, _id)
-	return demand - supply
-
-# returns dict with ids and coordinates
-def get_hive_locations():
-	all_hives = rest.get_all_hives()
-	hives = dict()
-	for hive in all_hives:
-		hives[hive['id']] = Point(hive['xcoord'], hive['ycoord'])
-	return hives;
-
-def get_all_hive_ids():
-	all_hives = rest.get_all_hives()
-	hives = []
-	for hive in all_hives:
-		hives.append(hive['hive']['id'])
-	return hives
-
-def get_drones_of_hive(_id):
-	drones_of_hive = rest.get_drones_of_hive(_id)
-	hives = []
-	for hive in drones_of_hive:
-		hives.append(hive['id'])
-	return hives
-
-def get_hives_with_drones():
-	all_drones = rest.get_all_drones()
-	all_hives = rest.get_all_hives()
-	hives = dict()
-	for hive in all_hives:
-		number_of_drones = 0
-		for drone in all_drones:
-			if (hive['hive']['id'] == drone['hive']['id']):
-				number_of_drones += 1
-		hives[hive['hive']['id']] = number_of_drones
-	return hives
-
-#for testing purposes
-def set_drones_for_hive(hiveid, amount):
-	#drones = []
-	for nr in range(amount):
-		drone = dict()
-		drone['hiveid'] = hiveid
-		drone['name'] = "drone-"+str(nr)
-		drone['status'] = ":drone.status/idle"
-		#drones.append(drone)
-		rest.post_hive_drone(json.dumps(drone, ensure_ascii=False))
-
-def set_d_of_hive(hiveid, hives):
-	incoming = get_incoming_drones(hiveid, hives)
-	outgoing = get_outgoing_drones(hiveid, hives)
-	io_ratio = incoming / outgoing
-	drone_demand = get_drone_demand(hiveid, hives)
-	demand = dict()
-	demand['demand'] = drone_demand
-	demand = json.dumps(demand, ensure_ascii=False)
-	rest.post_desired_drones_of_hive(hiveid, demand)
-
-# TODO: error code handling
-def get_incoming_drones(hiveid, hives):
-	for hive in hives:
-		if (hives['hive']['id'] == hiveid):
-			return hives['hive']['i']
-	return 99999
-
-def get_outgoing_drones(hiveid, hives):
-	for hive in hives:
-		if (hives['hive']['id'] == hiveid):
-			return hives['hive']['o']
-	return 99999
-
-def get_drone_demand(hiveid, hives):
-	for hive in hives:
-		if (hives['hive']['id'] == hiveid):
-			return hives['hive']['demand']
-	return 99999
-
-# ---------------------------- based on domains
-def get_all_hives():
-	all_hives = rest.get_all_hives()
-	hives = []
-	for hive in hives:
-		hivedomain = get_hivedomain(hive)
-		hives.append(domain)
-	return hives
-
-def get_hivedomain(json):
-	hive = Hive()
-	hive.id = json['hive']['id']
-	hive.name = json['hive']['name']
-	hive.demand = json['hive']['demand']
-	hive.validate()
-	return hive
-
 ### ---------------------------------------- OPTIONAL
-
 def get_impact_to(_id, drones):
 	future_orders = get_orders_in(droneservice.get_time_of_impact(), _id)
 	future_drones = get_drones_in(droneservice.get_time_of_impact(), _id)
--- tests/service/hiveservice_test.py
+++ tests/service/hiveservice_test.py
@@ -1,8 +1,64 @@
 #!/usr/bin/env python3
 import json
+from pytest import fixture
 from unittest.mock import patch
+from distribution.domain.hive import Hive
+from distribution.domain.drone import Drone
 from distribution.domain.point import Point
-from distribution.service import hiveservice, droneservice
+from distribution.service import hiveservice, buildingservice, droneservice
+
+@fixture
+def json_buildings():
+	return [{"id": 1,"address": "Karlsplatz",
+			"xcoord": 16,"ycoord": 48,
+			"hive":{"id": 11,"name": "Karlsplatz",
+				"demand": -1, "free": 1}},
+			{"id": 2,"address": "Westbahnhof",
+			"xcoord": 32,"ycoord": 11,
+			"hive":{"id": 12,"name": "Westbahnhof",
+				"demand": -1, "free": 3}},
+			{"id": 3,"address": "Stephansplatz",
+			"xcoord": 2,"ycoord": 21,
+			"hive":{"id": 13,"name": "Stephansplatz",
+				"demand": -1, "free": 8}}]
+
+@fixture
+def json_hives():
+	return [{"id": 11,"name": "Karlsplatz", "free": 1},
+			{"id": 12,"name": "Westbahnhof", "free": 3}]
+
+@fixture
+def domain_hives():
+	return [Hive({"id": 11,"name": "Karlsplatz", "demand": -1, "free": 1}),
+			Hive({"id": 12,"name": "Westbahnhof", "demand": -1, "free": 3}),
+			Hive({"id": 13,"name": "Stephansplatz", "demand": -1, "free": 8})]
+
+@fixture
+def json_reachable():
+	return 	[{'id': 1111,'start': {'id': 1},
+				'end': {'id': 2},'distance': 3000},
+			{'id': 2222,'start': {'id': 3},
+				'end': {'id': 1},'distance': 2500},
+			{'id': 3333,'start': {'id': 2},
+				'end': {'id': 3},'distance': 2800}]
+
+@fixture
+def json_drones():
+	return [{"id": 101,"hive": {"id": 11},"name": "drone01",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}},
+			{"id": 102,"hive": {"id": 12},"name": "drone02",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}},
+			{"id": 103,"hive": {"id": 11},"name": "drone03",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}}]
+
+@fixture
+def domain_drones():
+	return [Drone({"id": 101,"hive": {"id": 11},"name": "drone01",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}}),
+			Drone({"id": 102,"hive": {"id": 12},"name": "drone02",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}}),
+			Drone({"id": 103,"hive": {"id": 11},"name": "drone03",
+				"type": {"id": 0},"status": {"ident": "drone.status/idle"}})]
 
 @patch('distribution.service.hiveservice.get_drones_in')
 @patch('distribution.service.hiveservice.get_orders_in')
@@ -31,42 +87,11 @@ def test_amount_of_drones_for(mock_workload, mock_orders,
 
 @patch('distribution.rest.rest.get_reachable_buildings')
 def test_get_reachable_buildings(mock_reachable):
-	reachable = [{	'id': 1,'start': {'id': 11},
-					'end': {'id': 13},'distance': 3000},
-					{'id': 2,'start': {'id': 13},
-					'end': {'id': 12},'distance': 2500},
-					{'id': 3,'start': {'id': 12},
-					'end': {'id': 11},'distance': 2500}]
-	reachable_buildings = json.dumps(reachable)
+	reachable_buildings = json.dumps(json_reachable())
 	mock_reachable.return_value = json.loads(reachable_buildings)
-	reachable_buildings = hiveservice.get_reachable_buildings(13)
-	expected_hives = [ 11, 12 ]
+	reachable_buildings = buildingservice.get_reachable_buildings(3)
+	expected_hives = [ 1, 2 ]
 	assert reachable_buildings == expected_hives
-
-@patch('distribution.rest.rest.get_reachable_buildings')
-@patch('distribution.rest.rest.get_all_hives')
-def test_get_reachable_hives(mock_hives, mock_reachable):
-	all_hives = [	{"id": 1,"address": "Karlsplatz",
-					"xcoord": 16,"ycoord": 48,"hive":{
-					"id": 11,"name": "Karlsplatz"}},
-					{"id": 2,"address": "Westbahnhof",
-					"xcoord": 32,"ycoord": 11,"hive":{
-					"id": 12,"name": "Westbahnhof"}},
-					{"id": 3,"address": "Westbahnhof",
-					"xcoord": 32,"ycoord": 11,"hive":{
-					"id": 13,"name": "Westbahnhof"}}]
-	mock_hives.return_value = all_hives
-	reachable = [	{'id': 1111,'start': {'id': 1},
-					'end': {'id': 2},'distance': 3000},
-					{'id': 2222,'start': {'id': 3},
-					'end': {'id': 1},'distance': 2500},
-					{'id': 3333,'start': {'id': 2},
-					'end': {'id': 3},'distance': 2800}]
-	reachable_buildings = json.dumps(reachable)
-	mock_reachable.return_value = json.loads(reachable_buildings)
-	reachable_hives = hiveservice.get_reachable_hives(12)
-	expected_hives = [ 11, 13 ]
-	assert reachable_hives == expected_hives
 
 @patch('distribution.rest.rest.get_drones_in')
 def test_get_drones_in(mock_drones):
@@ -81,15 +106,7 @@ def test_orders_in(mock_orders):
 	assert orders == 10
 
 def test_get_free_drones():
-	all_hives = [
-		{"id": 1,"address": "Karlsplatz",
-			"xcoord": 16,"ycoord": 48,"hive":
-			{"id": 11,"name": "Karlsplatz", "free": 1}},
-		{"id": 2,"address": "Westbahnhof",
-			"xcoord": 32,"ycoord": 11,"hive":
-		{"id": 12,"name": "Westbahnhof", "free": 3}
-		}]
-	free_drones = hiveservice.get_free_drones(2, all_hives)
+	free_drones = hiveservice.get_free_drones(12, json_hives())
 	expected_drones = 3
 	assert free_drones == expected_drones
 
@@ -126,9 +143,9 @@ def test_get_drones_to_send_eotd_true(mock_needed_drones):
 	assert needed_drones == 10
 
 @patch('distribution.service.hiveservice.get_free_drones')
-@patch('distribution.rest.rest.get_all_hives')
-def test_get_drones_to_send_eotd_false(mock_hives, mock_free_drones):
-	mock_hives.return_value = ""
+@patch('distribution.rest.rest.get_all_buildings')
+def test_get_drones_to_send_eotd_false(mock_buildings, mock_free_drones):
+	mock_buildings.return_value = ""
 	mock_free_drones.return_value = 10
 	drones_without_impact = hiveservice.get_drones_to_send(1, False)
 	assert drones_without_impact == 10
@@ -165,70 +182,26 @@ def test_get_needed_drones_negative(mock_demand, mock_supply):
 	expected_drones = -10
 	assert drones == expected_drones
 
-@patch('distribution.rest.rest.get_all_hives')
-def test_get_hive_locations(mock_hives):
-	all_hives = [
-		{"id": 1,"address": "Karlsplatz",
-			"xcoord": 16,"ycoord": 48,"hive":
-			{"id": 11,"name": "Karlsplatz"}},
-		{"id": 2,"address": "Westbahnhof",
-			"xcoord": 32,"ycoord": 11,"hive":
-		{"id": 12,"name": "Westbahnhof"}
-		}]
-	mock_hives.return_value = all_hives
-	hives = hiveservice.get_hive_locations()
-	expected_hives = { 1:Point(16,48), 2:Point(32,11) }
-	assert len(hives) == len(expected_hives)
+@patch('distribution.service.droneservice.get_all_drones')
+def test_get_drones_of_hive(mock_drones):
+	mock_drones.return_value = domain_drones()
+	drones = hiveservice.get_drones_of_hive(11)
+	expected_drones = [ domain_drones()[0], domain_drones()[2] ]
+	assert_domain_lists(drones, expected_drones)
 
-@patch('distribution.rest.rest.get_drones_of_hive')
-def test_get_drones_of_hive(mock_drones_of_hive):
-	drones_of_hive = [
-		{"id": 1,"name": "drone01","type": {"id": 17592186045434},
-			"status": {"id": 17592186045421}},
-		{"id": 2,"name": "drone02","type": {"id": 17592186045434},
-			"status": {"id": 17592186045421}}]
-	mock_drones_of_hive.return_value = drones_of_hive
-	drones = hiveservice.get_drones_of_hive(0)
-	expected_drones = [ 1, 2 ]
-	assert drones == expected_drones
-
-@patch('distribution.rest.rest.get_all_hives')
-def test_get_all_hive_ids(mock_hives):
-	all_hives = [
-		{"id": 1,"address": "Karlsplatz",
-			"xcoord": 16,"ycoord": 48,"hive":
-			{"id": 11,"name": "Karlsplatz"}},
-		{"id": 2,"address": "Westbahnhof",
-			"xcoord": 32,"ycoord": 11,"hive":
-		{"id": 12,"name": "Westbahnhof"}
-		}]
-	mock_hives.return_value = all_hives
+@patch('distribution.rest.rest.get_all_buildings')
+def test_get_all_hive_ids(mock_buildings):
+	mock_buildings.return_value = json_buildings()
 	hives = hiveservice.get_all_hive_ids()
-	expected_hives = [ 11, 12 ]
+	expected_hives = [ 11, 12, 13 ]
 	assert hives == expected_hives
 
 @patch('distribution.rest.rest.get_all_drones')
-@patch('distribution.rest.rest.get_all_hives')
-def test_get_hives_with_drones(mock_hives, mock_drones):
-	all_hives = [
-		{"id": 1,"address": "Karlsplatz",
-			"xcoord": 16,"ycoord": 48,"hive":
-			{"id": 11,"name": "Karlsplatz"}},
-		{"id": 2,"address": "Westbahnhof",
-			"xcoord": 32,"ycoord": 11,"hive":
-			{"id": 12,"name": "Westbahnhof"}
-		}]
-	all_drones = [
-		{"id": 101,"hive": {"id": 11},"name": "drone01",
-			"type": {"id": 0},"status": {"ident": "IDLE"}},
-		{"id": 102,"hive": {"id": 12},"name": "drone02",
-			"type": {"id": 0},"status": {"ident": "IDLE"}},
-		{"id": 103,"hive": {"id": 11},"name": "drone03",
-			"type": {"id": 0},"status": {"ident": "IDLE"}
-		}]
-	mock_hives.return_value = all_hives
-	mock_drones.return_value = all_drones
-	hives_with_drones = hiveservice.get_hives_with_drones()
+@patch('distribution.service.hiveservice.get_all_hives')
+def test_get_hives_with_dronecount(mock_hives, mock_drones):
+	mock_hives.return_value = domain_hives()
+	mock_drones.return_value = json_drones()
+	hives_with_drones = hiveservice.get_hives_with_dronecount()
 	expected_hives_with_drones = { 11:2, 12:1 }
 	expected_length = len(expected_hives_with_drones)
 	length = 0
@@ -237,3 +210,25 @@ def test_get_hives_with_drones(mock_hives, mock_drones):
 			if (hives_with_drones[hive] == expected_hives_with_drones[hive]):
 				length += 1
 	assert length == expected_length
+
+# -------------- REFACTOR
+@patch('distribution.rest.rest.get_all_buildings')
+def test_get_all_hives(mock_buildings):
+	mock_buildings.return_value = json_buildings()
+	hives = hiveservice.get_all_hives()
+	hive11 = domain_hives()[0]#Hive({'id': 11, 'name': 'Karlsplatz', 'demand': -1, 'free': 1})
+	hive12 = domain_hives()[1]#Hive({'id': 12, 'name': 'Westbahnhof', 'demand': -1, 'free': 3})
+	hive13 = domain_hives()[2]#Hive({'id': 13, 'name': 'Stephansplatz', 'demand': -1, 'free': 8})
+	expected_hives = [ hive11, hive12, hive13 ]
+	assert_domain_lists(hives, expected_hives)
+
+def test_get_hivedomain():
+	hive = {"id": 11,"name": "Karlsplatz", "demand": -1}
+	hivedomain = hiveservice.get_hivedomain(hive)
+	expected_hivedomain = Hive(hive)
+	assert hivedomain.to_primitive() == expected_hivedomain.to_primitive()
+
+def assert_domain_lists(domains, expected_domains):
+	assert len(domains) == len(expected_domains)
+	for i in range(len(domains)):
+		assert domains[i].to_primitive() == expected_domains[i].to_primitive()

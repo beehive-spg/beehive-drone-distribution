80e20b952d963a2a3d7e50ac99e61bf23a614ea3
Dschonas <jonasneumair@gmail.com>
2018-02-11 21:05:24 +0100

    DLN-284 #time 20m Buildingservice test adaptions

--- tests/service/buildingservice_test.py
+++ tests/service/buildingservice_test.py
@@ -3,11 +3,10 @@ import json
 from pytest import fixture
 from unittest.mock import patch
 from distribution.domain.building import Building
-from distribution.domain.reachable import Reachable
 from distribution.service import buildingservice
 
 @fixture
-def all_buildings():
+def json_buildings():
 	return [{"id": 1,"address": "Karlsplatz",
 			"xcoord": 16,"ycoord": 48,"hive":
 			{"id": 11,"name": "Karlsplatz", "demand": -1}},
@@ -16,16 +15,13 @@ def all_buildings():
 			{"id": 12,"name": "Westbahnhof", "demand": -1}}]
 
 @fixture
-def building1():
-	return Building({"id": 1,"address": "Karlsplatz",
+def domain_buildings():
+	return [Building({"id": 1,"address": "Karlsplatz",
 						"xcoord": 16,"ycoord": 48,"hive":
-						{"id": 11,"name": "Karlsplatz", "demand": -1}})
-
-@fixture
-def building2():
-	return Building({"id": 2,"address": "Westbahnhof",
+						{"id": 11,"name": "Karlsplatz", "demand": -1}}),
+			Building({"id": 2,"address": "Westbahnhof",
 						"xcoord": 32,"ycoord": 11,"hive":
-						{"id": 12,"name": "Westbahnhof", "demand": -1}})
+						{"id": 12,"name": "Westbahnhof", "demand": -1}})]
 
 @fixture
 def all_reachable():
@@ -36,27 +32,16 @@ def all_reachable():
 			{'id': 3333,'start': {'id': 2},
 				'end': {'id': 3},'distance': 2800}]
 
-@fixture
-def reachable1():
-	return Reachable({'id': 1111,'start': {'id': 1},
-						'end': {'id': 2},'distance': 3000})
-
-@fixture
-def reachable2():
-	return Reachable({'id': 2222,'start': {'id': 3},
-						'end': {'id': 1},'distance': 2500})
-
-
 @patch('distribution.rest.rest.get_all_buildings')
 def test_get_all_buildings(mock_hives):
-	mock_hives.return_value = all_buildings()
+	mock_hives.return_value = json_buildings()
 	buildings = buildingservice.get_all_buildings()
-	expected_buildings = [ building1(), building2() ]
+	expected_buildings = [ domain_buildings()[0], domain_buildings()[1] ]
 	assert_domain_lists(buildings, expected_buildings)
 
 def test_get_buildingdomain():
-	buildingdomain = buildingservice.get_buildingdomain(building1())
-	expected_buidling = Building(building1())
+	buildingdomain = buildingservice.get_buildingdomain(domain_buildings()[0])
+	expected_buidling = Building(domain_buildings()[0])
 	assert buildingdomain.to_primitive() == expected_buidling.to_primitive()
 
 @patch('distribution.rest.rest.get_reachable_buildings')

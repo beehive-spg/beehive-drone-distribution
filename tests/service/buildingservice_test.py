b56ab9fe566d660c3e9d552b139a4d2c6070329b
Dschonas <jonasneumair@gmail.com>
2018-02-11 17:05:59 +0100

    DLN-284 #time 10m Adjust buildingservice test

--- tests/service/buildingservice_test.py
+++ tests/service/buildingservice_test.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
+from pytest import fixture
 from unittest.mock import patch
 from distribution.domain.building import Building
 from distribution.service import buildingservice
 
-@patch('distribution.rest.rest.get_all_buildings')
-def test_get_all_buildings(mock_hives):
-	all_hives = [
-		{"id": 1,"address": "Karlsplatz",
+@fixture
+def all_buildings():
+	return [{"id": 1,"address": "Karlsplatz",
 			"xcoord": 16,"ycoord": 48,"hive":
 			{"id": 11,"name": "Karlsplatz", "demand": -1}},
-		{"id": 2,"address": "Westbahnhof",
+			{"id": 2,"address": "Westbahnhof",
 			"xcoord": 32,"ycoord": 11,"hive":
-			{"id": 12,"name": "Westbahnhof", "demand": -1}
-		}]
-	mock_hives.return_value = all_hives
-	buildings = buildingservice.get_all_buildings()
-	building1 = Building({"id": 1,"address": "Karlsplatz",
+			{"id": 12,"name": "Westbahnhof", "demand": -1}}]
+
+@fixture
+def building1():
+	return Building({"id": 1,"address": "Karlsplatz",
 						"xcoord": 16,"ycoord": 48,"hive":
 						{"id": 11,"name": "Karlsplatz", "demand": -1}})
-	building2 = Building({"id": 2,"address": "Westbahnhof",
+
+@fixture
+def building2():
+	return Building({"id": 2,"address": "Westbahnhof",
 						"xcoord": 32,"ycoord": 11,"hive":
 						{"id": 12,"name": "Westbahnhof", "demand": -1}})
-	expected_buildings = [ building1, building2 ]
-	assert_two_domain_lists(buildings, expected_buildings)
+
+@patch('distribution.rest.rest.get_all_buildings')
+def test_get_all_buildings(mock_hives):
+	mock_hives.return_value = all_buildings()
+	buildings = buildingservice.get_all_buildings()
+	expected_buildings = [ building1(), building2() ]
+	assert_domain_lists(buildings, expected_buildings)
 
 def test_get_buildingdomain():
-	building = {"id": 1,"address": "Karlsplatz",
-			"xcoord": 16,"ycoord": 48,"hive":
-			{"id": 11,"name": "Karlsplatz", "demand": -1}}
-	buildingdomain = buildingservice.get_buildingdomain(building)
-	expected_buidling = Building(building)
+	buildingdomain = buildingservice.get_buildingdomain(building1())
+	expected_buidling = Building(building1())
 	assert buildingdomain.to_primitive() == expected_buidling.to_primitive()
 
-def assert_two_domain_lists(domain1, domain2):
-	assert len(domain1) == len(domain2)
-	for i in range(len(domain1)):
-		assert domain1[i].to_primitive() == domain2[i].to_primitive()
+def assert_domain_lists(domains, expected_domains):
+	assert len(domains) == len(expected_domains)
+	for i in range(len(domains)):
+		assert domains[i].to_primitive() == expected_domains[i].to_primitive()

e9619061c59c38fcfdac243f2a3f0fc9b240d84e
Dschonas <jonasneumair@gmail.com>
2018-03-10 13:54:51 +0100

    DLN-278 #time 2h Implement custom logger

--- distribution/foundation/logger.py
+++ distribution/foundation/logger.py
@@ -1,8 +1,31 @@
-def info(message):
-    logger.info(message)
+import logging
 
-def warning(message):
-    logger.warning(message)
+class Logger():
 
-def critical(message):
-    logger.critical(message)
+    def __init__(self, name):
+        self.name = name
+        self.logger = self.getLogger()
+
+    def getLogger(self):
+        disable_logger = [ 'pika' ]
+
+        for logger in disable_logger:
+            logging.getLogger(logger).setLevel(logging.CRITICAL)
+
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.INFO)
+
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        return logger
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
